c36225846570da9bfb2b18e98f58b0c39d3956c5
Diogo Carvalho <diogocarvalhofarias@gmail.com>
2026-08-20 17:14:45 -0300

    fix: listen on 0.0.0.0

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.post("/commands/:id/result", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Bridge rodando na porta ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Bridge rodando na porta ${PORT}`));
